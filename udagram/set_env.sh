fc4ee498ecc8f3d7167bad072d07de58cf0542ba
mw <martinez2@gmx.de>
2022-06-30 09:07:53 +0200

    refactor: Replace forked credentials by dummies

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
+export POSTGRES_USERNAME=myUsername
 export POSTGRES_PASSWORD=myPassword
-export POSTGRES_HOST=mydbinstance.csxbuclmtj3c.us-east-1.rds.amazonaws.com
-export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::myawsbucket-75139724085
+export POSTGRES_HOST=myDatabaseEndpoint
+export POSTGRES_DB=myDatabase
+export AWS_BUCKET=arn:aws:s3:::myAwsBucket
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
-export JWT_SECRET=mysecretstring
+export JWT_SECRET=mySecretString
 export URL=http://localhost:8100
