a79793656c84e484773a8c236161cce023d2dc3c
mw <martinez2@gmx.de>
2022-07-05 10:14:25 +0200

    refactor: Update apiHost

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://udagramapi-env.eba-mqkpvptm.us-east-1.elasticbeanstalk.com/api/v0'
+  apiHost: 'http://udagramapi-env.eba-ztjrtare.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
